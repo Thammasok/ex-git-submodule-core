2fb539e673c1949760a9b66ac5da7134f65d49ff
jaranchai <jaranchai.nt@hotmail.com>
2024-04-25 09:49:10 +0700

    [Added] git module change

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const moduleA = require('./ex-git-submodule-a');
+const moduleB = require('./ex-git-submodule-b');
+
 const main = () => {
   console.log('Hello Core!');
+  
+  // call from module b
+  const text = moduleB.sayHello('B');
+  console.log(text);
+
+  // call from module a
+  const result = moduleA.add(1,3);
+  console.log(result);
 }
 
 main();
